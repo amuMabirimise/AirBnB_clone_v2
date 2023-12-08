c2394719f5749e77b7cf7a213392e9de19f71f90
root <amuMabirimise>
2023-12-08 03:55:20 -0800

    advanced

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,24 +3,20 @@
 import os
 from fabric.api import env, local, run
 
-env.hosts = ['34.239.250.75', '100.25.17.31', '100.25.31.110']
+env.hosts = ['34.239.250.75', '100.25.17.31']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+
 
 def do_clean(number=0):
-    """
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keep only the most recent archive.
-    If number is 2, keep the most and second most recent archives,
-    etc.
-    """
+    """ CLEANS """
+
     number = int(number)
-    number = 2 if number < 1 else number + 1
 
-    local("ls -d -1tr versions/* | tail -n +{} | \
-    xargs -d '\n' rm -f --".format(number))
+    if number == 0:
+        number = 2
+    else:
+        number += 1
 
-    run("ls -d -1tr /data/web_static/releases/* | tail -n +{} | \
-    xargs -d '\n' rm -rf --".format(number))
-
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
